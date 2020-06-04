9bdcb74ded82b9f841423df3ea06f94403c4c26d
HAYAMA_Kaoru <iyahaya@nifty.com>
2020-06-04 23:31:44 +0900

    Support UNIX

---
+++ example_unix/example.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+
+	"github.com/zetamatta/go-windows-fileid"
+)
+
+func mains() error {
+	id1, err := fileid.Query("../main_test.go")
+	if err != nil {
+		return err
+	}
+	fmt.Printf("fileid.Query=%d\n", id1)
+	cmd := exec.Command("ls", "-i", "../main_test.go")
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	return cmd.Run()
+}
+
+func main() {
+	if err := mains(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
--- main_unix.go
+++ main_unix.go
@@ -4,8 +4,18 @@ package fileid
 
 import (
 	"errors"
+	"os"
+	"syscall"
 )
 
 func queryFilenameById(fname string) (uint64, error) {
-	return 0, errors.New("not support")
+	fi, err := os.Stat(fname)
+	if err != nil {
+		return 0, err
+	}
+	stat, ok := fi.Sys().(*syscall.Stat_t)
+	if !ok {
+		return 0, errors.New("os.Fileinfo.Sys() is not syscall.Stat_t")
+	}
+	return stat.Ino, nil
 }
